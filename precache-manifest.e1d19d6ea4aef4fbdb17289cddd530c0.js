43f0994139c89646eaec0e72a8ae1297a0ab714d
Andrej Stevkovski <stevandrej@yahoo.com>
2020-05-21 04:16:31 +0200

    Updates

---
+++ precache-manifest.e1d19d6ea4aef4fbdb17289cddd530c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7100ef8af9eb29d25d9bfeb5e08b260",
+    "url": "/movie_searcher/index.html"
+  },
+  {
+    "revision": "07a63a2b4be2d6526d4a",
+    "url": "/movie_searcher/static/css/main.9d971d2d.chunk.css"
+  },
+  {
+    "revision": "1c3035c193932872ce26",
+    "url": "/movie_searcher/static/js/2.7ffd7640.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movie_searcher/static/js/2.7ffd7640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a63a2b4be2d6526d4a",
+    "url": "/movie_searcher/static/js/main.29fea9a4.chunk.js"
+  },
+  {
+    "revision": "657c5b27c74837076f07",
+    "url": "/movie_searcher/static/js/runtime-main.36c3315d.js"
+  }
+]);
